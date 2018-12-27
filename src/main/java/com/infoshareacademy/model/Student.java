a12f73b5f541c42fc8887e484ca0888b1db14d84
Daniel Modrzejewski <danio1118m@gmail.com>
2018-12-27 12:38:19 +0100

    ad 1

--- src/main/java/com/infoshareacademy/model/Student.java
+++ src/main/java/com/infoshareacademy/model/Student.java
@@ -2,20 +2,7 @@ package com.infoshareacademy.model;
 
 import java.time.LocalDate;
 import java.util.List;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
---
+++ src/main/java/com/infoshareacademy/service/StudentService.java
@@ -0,0 +1,47 @@
+package com.infoshareacademy.service;
+
+
+import com.infoshareacademy.dao.StudentDao;
+import com.infoshareacademy.model.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+@Transactional
+@Path("/api/")
+public class StudentService {
+
+    private Logger LOG = LoggerFactory.getLogger(StudentService.class);
+
+    @Context
+    private UriInfo uriInfo;
+
+    @Inject
+    private StudentDao studentDao;
+
+    public StudentService() {
+    }
+
+    @GET
+    @Path("/students")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sayHello() {
+        String resp = new String();
+        final List<Student> result = studentDao.findAll();
+        LOG.info("Found {} objects", result.size());
+        for (Student p : result) {
+            resp+=(p.toString() + "\n");
+        }
+        return Response.ok(resp).build();
+    }
+}
